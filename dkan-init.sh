ffcb90172681f479631738a212e90e611650e00c
Frank Carey <frank@devinci.io>
2016-01-05 16:12:03 -0500

    Have the dkan-init.sh setup the initial ahoy file and check that ahoy is installed.

--- dkan-init.sh
+++ dkan-init.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
 mkdir dkan &&
 #mv !(dkan) dkan/.
 #find . ! -regex '.*/dkan' ! -regex '.' -exec mv '{}' dkan \; &&
-shopt -s extglob dotglob
-mv !(dkan) dkan
-shopt -u dotglob
+shopt -s extglob dotglob &&
+mv !(dkan) dkan &&
+shopt -u dotglob &&
 # find . ! -regex '.*/dkan' ! -regex '.' -exec echo "'{}'" \; &&
-ahoy init
+cp dkan/.ahoy/starter.ahoy.yml .ahoy.yml &&
+echo "A DKAN Drupal site has been initialized. Type 'ahoy' for DKAN commands."
+which ahoy || echo "Notice: ahoy is not installed. Follow the instructions at https://github.com/devinci-code/ahoy to install it."
